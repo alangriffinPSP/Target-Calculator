0d733d17f391cbe94477f29ece063e6bb6e136a7
Alan Griffin <alan.griffin@petsitterplus.com>
2025-01-26 17:57:24 +0000

    Added formatting to longer numbers.

--- targetCalc.js
+++ targetCalc.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
                 $('#warning').css('display', 'inline-block');
                 $('#warning').text("Numbers only, please");
                 return;
-            } 
+            }
 
             const deficit = Math.round(parseInt($inputs.target) - parseInt($inputs.actual))
             const week = Math.round(deficit / parseInt($inputs.weeks))
@@ -30,12 +30,12 @@ $(document).ready(function () {
             if (deficit > 0) {
                 $('#calculateButton').css('display', 'none');
                 $('#results').css('display', 'inline-block');
-                $('#deficit').text(` £${deficit} `);
+                $('#deficit').text(` £${deficit.toLocaleString()} `);
                 $('#percentage').text(`(${percentageDifference.toFixed(1)}%)`);
                 $('#weeksLeft').text(` ${$inputs.weeks} `);
-                $('#targetWeek').text(`£${week} Per Week`);
-                $('#targetDay').text(`£${day} Per Day`);
-                $('#targetHour').text(`£${hour} Per Hour`);
+                $('#targetWeek').text(`£${week.toLocaleString()} Per Week`);
+                $('#targetDay').text(`£${day.toLocaleString()} Per Day`);
+                $('#targetHour').text(`£${hour.toLocaleString()} Per Hour`);
                 console.log($inputs, deficit, week, day, hour, percentageDifference);
             } else {
                 $('#congrats').css('display', 'inline-block');
